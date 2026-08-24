f306216cc44a03a72c874330a8f9688ead941164
Frank Ellison <frankdellison@gmail.com>
2026-08-24 16:09:58 -0400

    Add a security filter chain so we can see the redirect before showing the page.

--- src/main/java/com/dev/oauth/OauthApplication.java
+++ src/main/java/com/dev/oauth/OauthApplication.java
@@ -5,8 +5,13 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +23,22 @@ public class OauthApplication {
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeHttpRequests(a -> a
+				.requestMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.oauth2Login();
+		// @formatter:on
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
